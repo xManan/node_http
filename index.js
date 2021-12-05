399b9dda84db77cb2183e9c416fa7901371b9c03
Manan <mananchawla10@gmail.com>
2021-12-05 14:14:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const server = http.createServer((req, res)=>{
+    if(req.url ==='/')
+    {
+        res.write('MAIN')
+        res.end()
+    }
+});
+
+server.listen(3000, ()=>console.log(`listening on port 3000...`));
+
+server.on('connection', connection=>console.log("New conn: ", connection.remoteAddress+":"+ connection.remotePort));
+
+server.on('request', (req,  res)=>{
+	if(req.url === '/req'){
+		res.write('REQ')
+		res.end()
+	}
+})
